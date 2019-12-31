8bdff5aafe727815cd2b9bda3e56a0f09a263373
Sara Fotros <sarafotros@users.noreply.github.com>
2019-12-31 11:23:43 +0000

    user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
     has_many :reviews, through: :tickets
     has_many :events, through: :tickets
     has_many :organiser, through: :events
+
+    def self.create_account(name)
+        User.create(name:name)
+    end
+
+    def self.user_login(name)
+        User.find_by(name:name)
+    end
+
+    def self.delete_user(name)
+        user = user_login(name)
+        User.destroy(user.id)
+    end
+
 end
