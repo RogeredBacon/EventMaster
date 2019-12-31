a387cfbdaf475a7ada6dea1e45a0522afaf566c3
James <jinglis12@googlemail.com>
2019-12-31 11:31:36 +0000

    Removed duplicate migration files

--- db/migrate/20191230134853_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.boolean :organiser?
-      t.timestamp :created_on
-    end
-  end
-end
