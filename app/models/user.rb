d837fa0c89fd4fa4da07d043ffa40bd390e60a48
James <jinglis12@googlemail.com>
2019-12-31 11:14:29 +0000

    Added queries for user, #14 #13 #5 #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
-has_many :tickets
-has_many :reviews
-has_many :tickets, through: :events
+    has_many :tickets
+    has_many :reviews
+    has_many :tickets, through: :events
+
+    def self.add_new_user(name, organiser_bool)
+        User.create(name: name, organiser?: organiser_bool)
+    end
+
+    def self.login_user(name, organiser_bool)
+        User.find_by(name: name, organiser?: organiser_bool)
+    end
+
+    def self.delete_user(name, organiser_bool)
+        user = login_user(name, organiser_bool)
+        User.destroy(user.id)
+    end
 end
